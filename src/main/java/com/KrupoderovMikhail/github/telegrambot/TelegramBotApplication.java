3788a52ddabd06bf21b46a18842b7247c5f9630d
Krupoderov Mikhail <krupoderov.mikhail@gmail.com>
2020-03-07 14:54:38 +0500

    Initial commit

---
+++ src/main/java/com/KrupoderovMikhail/github/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.KrupoderovMikhail.github.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+	public static void main(String[] args) {
+		ApiContextInitializer.init();
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/KrupoderovMikhail/github/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,15 @@
+package com.KrupoderovMikhail.github.telegrambot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TelegramBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
